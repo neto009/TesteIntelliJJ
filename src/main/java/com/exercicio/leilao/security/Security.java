b269a738ca7884b09dbc24f2665b149a6889dd1f
aluno <aluno@udicentro.local>
2022-05-16 22:35:51 -0300

    Security

---
+++ src/main/java/com/exercicio/leilao/security/Security.java
@@ -0,0 +1,24 @@
+package com.exercicio.leilao.security;
+
+import java.beans.Customizer;
+
+import javax.annotation.security.PermitAll;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class Security {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests().antMatchers("/h2-console/**").permitAll().anyRequest().authenticated()
+        .and()
+        .csrf((csrf)-> csrf.ignoringAntMatchers("/h2-console/**"))
+        .headers().frameOptions().disable();
+        return http.build();
+    }
+}
